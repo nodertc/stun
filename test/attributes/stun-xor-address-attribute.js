1cda8e314055e49c1879328ea05622e7939586da
reklatsmasters <reklatsmasters@users.noreply.github.com>
2018-07-25 04:26:25 +0500

    Incorrect xor operation with IPv6 in xor-address-attribute. Fixed #11.

--- test/attributes/stun-xor-address-attribute.js
+++ test/attributes/stun-xor-address-attribute.js
@@ -3,7 +3,7 @@ const constants = require('lib/constants')
 
 const type = constants.attributeType.XOR_MAPPED_ADDRESS
 
-test('encode', () => {
+test('encode ipv4', () => {
   const attr = new StunXorAddressAttribute(type, '192.168.1.35', 63524)
 
   const owner = {
@@ -19,5 +19,29 @@ test('encode', () => {
   })
 
   const expectedBuffer = Buffer.from('0001d936e1baa561', 'hex')
+  expect(attr.toBuffer()).toEqual(expectedBuffer)
+})
+
+test('encode ipv6', () => {
+  const attr = new StunXorAddressAttribute(type, 'fe80::1', 63524)
+
+  const owner = {
+    transactionId: Buffer.from('d00558707bb8cc6a633a9df7', 'hex')
+  }
+
+  attr.setOwner(owner)
+
+  expect(attr.value).toEqual({
+    address: 'fe80::1',
+    port: 63524,
+    family: 'IPv6'
+  })
+
+  const address = Buffer.alloc(16)
+  address[0] = 0xFE
+  address[1] = 0x80
+  address[15] = 1
+
+  const expectedBuffer = Buffer.from('0002d936df92a442d00558707bb8cc6a633a9df6', 'hex')
   expect(attr.toBuffer()).toEqual(expectedBuffer)
 })
