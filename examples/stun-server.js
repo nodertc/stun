2469f3cd13a5d740ca54e4e43cb6d444619c0e2d
Bryan Nielsen <bnielsen1965@gmail.com>
2020-01-15 13:04:38 -0700

    Fix bug in server example by adding request transactionId to response message.

--- examples/stun-server.js
+++ examples/stun-server.js
@@ -8,7 +8,7 @@ const { STUN_BINDING_RESPONSE, STUN_EVENT_BINDING_REQUEST } = stun.constants;
 const userAgent = `node/${process.version} stun/v1.0.0`;
 
 server.on(STUN_EVENT_BINDING_REQUEST, (request, rinfo) => {
-  const message = stun.createMessage(STUN_BINDING_RESPONSE);
+  const message = stun.createMessage(STUN_BINDING_RESPONSE, request.transactionId);
 
   message.addXorAddress(rinfo.address, rinfo.port);
   message.addSoftware(userAgent);
